8d32e059103240e2076b4b1d9b0dce821cdfcc08
KuangPF <1633397286@qq.com>
2022-12-14 22:27:43 +0800

    chore(deps): add @emotions css in js framework

--- example/.dumirc.ts
+++ example/.dumirc.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   // disable mfsu for HMR
   mfsu: false,
+  locales: [
+    { id: 'en-US', name: 'English', suffix: '' },
+    { id: 'zh-CN', name: '中文', suffix: '-cn' },
+  ],
+  extraBabelPresets: ['@emotion/babel-preset-css-prop'],
   // pass theme config
   themeConfig: {
     hello: 'world',
