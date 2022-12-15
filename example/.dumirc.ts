4e2f4cfe2a002e090a7feec53333bfc8ae3d606d
KuangPF <1633397286@qq.com>
2022-12-15 22:58:52 +0800

    feat(locales): 默认使用中文

--- example/.dumirc.ts
+++ example/.dumirc.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   // disable mfsu for HMR
   mfsu: false,
   locales: [
-    { id: 'en-US', name: 'English', suffix: '' },
-    { id: 'zh-CN', name: '中文', suffix: '-cn' },
+    { id: 'zh-CN', name: '中文', suffix: '' },
+    { id: 'en-US', name: 'English', suffix: '-en' },
   ],
   extraBabelPresets: ['@emotion/babel-preset-css-prop'],
   // pass theme config
