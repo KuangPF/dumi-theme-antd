f9b9cdf305c5d88b3a08b80a9bd68b3e13046a89
KuangPF <1633397286@qq.com>
2022-12-17 20:20:36 +0800

    chore: set eact/button-has-type off

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: require.resolve('@umijs/lint/dist/config/eslint'),
   rules: {
     '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+    'react/button-has-type': 'off',
   },
 };
