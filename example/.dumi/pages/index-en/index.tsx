9685a657f33a8568f731a5fff862c082468d98e6
KuangPF <1633397286@qq.com>
2022-12-25 15:51:41 +0800

    feat(pages): delete index and index-cn

--- example/.dumi/pages/index-en/index.tsx
+++
@@ -1,3 +0,0 @@
-import Homepage from '../index/index';
-
-export default Homepage;
--- example/.dumi/pages/index/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { type FC } from 'react';
-
-const Homepage: FC = () => {
-  return <div>自定义首页</div>;
-};
-
-export default Homepage;
