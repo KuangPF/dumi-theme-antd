296a3d5b34320abec47d256451ad0f2441f4b94f
KuangPF <1633397286@qq.com>
2023-01-07 09:26:17 +0800

    feat(not-found): not found 页面多语言处理

--- src/slots/NotFound/index.tsx
+++ src/slots/NotFound/index.tsx
@@ -1,6 +1,6 @@
 import { HomeOutlined } from '@ant-design/icons';
 import { Button, Result } from 'antd';
-import { Link } from 'dumi';
+import { FormattedMessage, Link } from 'dumi';
 import React from 'react';
 
 export interface NotFoundProps {
@@ -17,11 +17,11 @@ const NotFoundPage: React.FC<NotFoundProps> = () => {
         <Result
           status="404"
           title="404"
-          subTitle={'你访问的页面貌似不存在？'}
+          subTitle={<FormattedMessage id="app.not-found.subTitle" />}
           extra={
             <Link to={'/'}>
               <Button type="primary" icon={<HomeOutlined />}>
-                {'返回 Ant Design 首页'}
+                <FormattedMessage id="app.not-found.back-home" />
               </Button>
             </Link>
           }
