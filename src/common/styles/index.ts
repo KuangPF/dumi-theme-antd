3236e370c7cd93e7da7c3b6d992c3066c2dff29a
KuangPF <1633397286@qq.com>
2023-01-09 23:09:14 +0800

    style(golbal-style): add responsive style

--- src/common/styles/index.ts
+++ src/common/styles/index.ts
@@ -2,3 +2,4 @@ export { default as Common } from './Common';
 export { default as Highlight } from './Highlight';
 export { default as Markdown } from './Markdown';
 export { default as Reset } from './Reset';
+export { default as Responsive } from './Responsive';
