166cc9780cccfb5b0c431fe81f1c3cb3ee6c0561
KuangPF <1633397286@qq.com>
2023-01-13 20:41:27 +0800

    style(home-page): 首页标题适配移动端

--- src/layouts/HomePageLayout/index.tsx
+++ src/layouts/HomePageLayout/index.tsx
@@ -1,13 +1,15 @@
 import { css } from '@emotion/react';
 import { Button, Space, Typography } from 'antd';
 import { Link, useSiteData } from 'dumi';
-import { type FC } from 'react';
+import { useContext, type FC } from 'react';
 import useAdditionalThemeConfig from '../../hooks/useAdditionalThemeConfig';
 import useSiteToken from '../../hooks/useSiteToken';
+import SiteContext from '../../slots/SiteContext';
 import Features from './components/Features';
 
 const useStyle = () => {
   const { token } = useSiteToken();
+  const { isMobile } = useContext(SiteContext);
 
   return {
     mainContent: css`
@@ -21,19 +23,29 @@ const useStyle = () => {
         font-family: AliPuHui, ${token.fontFamily};
       }
     `,
-    title: css`
-      h1& {
-        margin-bottom: ${token.marginMD}px;
-        font-weight: 900;
-        font-size: 68px;
-      }
-    `,
+    title: isMobile
+      ? css`
+          h1& {
+            margin-bottom: ${token.margin}px;
+            font-weight: normal;
+            font-size: ${token.fontSizeHeading1 + 2}px;
+            line-height: ${token.lineHeightHeading2};
+          }
+        `
+      : css`
+          h1& {
+            margin-bottom: ${token.marginMD}px;
+            font-weight: 900;
+            font-size: 68px;
+          }
+        `,
   };
 };
 
 const Homepage: FC = () => {
   const style = useStyle();
   const { token } = useSiteToken();
+  const { isMobile } = useContext(SiteContext);
   const {
     themeConfig: { name },
   } = useSiteData();
@@ -42,13 +54,13 @@ const Homepage: FC = () => {
     <div css={style.mainContent}>
       {/* Image Left Top */}
       <img
-        style={{ position: 'absolute', left: 0, top: 0, width: 240 }}
+        style={{ position: 'absolute', left: isMobile ? -120 : 0, top: 0, width: 240 }}
         src="https://gw.alipayobjects.com/zos/bmw-prod/49f963db-b2a8-4f15-857a-270d771a1204.svg"
         alt="bg"
       />
       {/* Image Right Top */}
       <img
-        style={{ position: 'absolute', right: 120, top: 0, width: 240 }}
+        style={{ position: 'absolute', right: isMobile ? 0 : 120, top: 0, width: 240 }}
         src="https://gw.alipayobjects.com/zos/bmw-prod/e152223c-bcae-4913-8938-54fda9efe330.svg"
         alt="bg"
       />
