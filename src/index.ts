5be655dd4139c5602455855227012d77c6172fc2
dream2023 <1098626505@qq.com>
2023-05-26 20:55:09 +0800

    feat: 增加 `defineThemeConfig` 函数，用于 ts 类型

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { IAllThemeConfig } from './types';
+
+/**
+ * @description provider declaration of config files
+ * @param {ThemeConfig} config theme config
+ * @returns {ThemeConfig}
+ */
+export function defineThemeConfig(config: IAllThemeConfig): IAllThemeConfig {
+  return config;
+}
