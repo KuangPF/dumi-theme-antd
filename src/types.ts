0acb7ace4010f43be1b54b6ebc011fcf2c8eecba
dream2023 <1098626505@qq.com>
2023-05-26 20:56:39 +0800

    feat: 增加 `antdModeSidebar` 配置，用于支持多层级 sidebar

--- src/types.ts
+++ src/types.ts
@@ -32,8 +32,38 @@ interface IFeature {
   /** 特性具体描述 */
   details: string;
   /** css in js 样式 */
-  itemCss: SerializedStyles;
+  itemCss?: SerializedStyles;
 }
+
+// 分组类型，将 children 换位字符串数组
+
+export type AntdModeSidebarMenuItemType = {
+  title: string;
+  link: string;
+  target?: string;
+};
+
+export type AntdModeSidebarMenuChildrenType = string | AntdModeSidebarMenuItemType;
+export type AntdModeSubMenuType = {
+  children: AntdModeSidebarMenuChildrenType[];
+  title: string;
+};
+
+// antd 模式的 sidebar 相关类型
+export type AntdModeSidebarMenuGroupChildren = (
+  | AntdModeSubMenuType
+  | AntdModeSidebarMenuChildrenType
+)[];
+export type AntdModeSidebarMenuGroupType = {
+  type: 'group';
+  title: string;
+  children: AntdModeSidebarMenuGroupChildren;
+};
+export type AntdModeSidebarMenuType =
+  | AntdModeSubMenuType
+  | AntdModeSidebarMenuGroupType
+  | AntdModeSidebarMenuChildrenType;
+export type AntdModeSidebarMenuItems = AntdModeSidebarMenuType[];
 
 interface IDocVersion {
   [propName: string]: string;
@@ -51,6 +81,8 @@ export interface IBannerConfig {
 interface IAdditionalThemeConfig {
   /** github 链接 */
   github?: string;
+  /** 首页链接 */
+  homeLink?: string;
   /** 多语言额外配置，主要用于展示语言切换文字（只针对于两项多语言时） */
   localesEnhance?: ILocaleEnhance[];
   /** 配置首页首屏区域的大标题。 */
@@ -62,15 +94,19 @@ interface IAdditionalThemeConfig {
   /** 配置首页特性描述 */
   features?: IFeature[] | Record<string, IFeature[]>;
   /** sidebar group 模式路由 */
-  sidebarGroupModePath?: ISidebarGroupModePathItem[];
+  sidebarGroupModePath?: true | ISidebarGroupModePathItem[];
   /** 文档版本 */
   docVersions?: IDocVersion;
   /** 更多链接 */
   moreLinks?: IMoreLink[];
   /** banner 配置 */
   bannerConfig?: IBannerConfig;
+  /** antd 模式的 sidebar */
+  antdModeSidebar?: Record<string, AntdModeSidebarMenuItems>;
 }
 
-export interface IAllThemeConfig extends IThemeConfig, IAdditionalThemeConfig {}
+export interface IAllThemeConfig extends Omit<IThemeConfig, 'socialLinks'>, IAdditionalThemeConfig {
+  socialLinks?: IThemeConfig['socialLinks']; // https://github.com/umijs/dumi/pull/1694
+}
 
 export { IAdditionalThemeConfig, ILocaleEnhance, IFeature, IAction };
