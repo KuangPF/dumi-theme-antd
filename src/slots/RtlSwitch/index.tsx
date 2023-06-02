6b2188c7aa635feaa0f501798cc6e21be4718e78
KuangPF <1633397286@qq.com>
2023-06-03 01:10:54 +0800

    feat(LTR): 替换 LTR 图标为 SVG，更好适配暗黑主题

--- src/slots/RtlSwitch/index.tsx
+++ src/slots/RtlSwitch/index.tsx
@@ -1,10 +1,22 @@
 import { useContext, type FC } from 'react';
+import { css } from '@emotion/react';
 import type { SiteContextProps } from '../SiteContext';
 import SiteContext from '../SiteContext';
 import SwitchBtn from '../Header/SwitchBtn';
+import LTRIcon from '../../icons/LTRIcon';
+import RTLIcon from '../../icons/RTLIcon';
+
+const useStyle = () => {
+  return {
+    dataDirectionIcon: css`
+      width: 16px;
+    `
+  };
+};
 
 const RtlSwitch: FC = () => {
   const { direction, updateSiteConfig } = useContext<SiteContextProps>(SiteContext);
+  const { dataDirectionIcon } = useStyle();
 
   const onDirectionChange = () => {
     updateSiteConfig({ direction: direction !== 'rtl' ? 'rtl' : 'ltr' });
@@ -14,19 +26,9 @@ const RtlSwitch: FC = () => {
     <SwitchBtn
       onClick={onDirectionChange}
       value={direction === 'rtl' ? 2 : 1}
-      label1={
-        <img
-          src="https://mdn.alipayobjects.com/huamei_7uahnr/afts/img/A*6k0CTJA-HxUAAAAAAAAAAAAADrJ8AQ/original"
-          alt="direction"
-        />
-      }
+      label1={<LTRIcon css={dataDirectionIcon} />}
       tooltip1="LTR"
-      label2={
-        <img
-          src="https://mdn.alipayobjects.com/huamei_7uahnr/afts/img/A*SZoaQqm2hwsAAAAAAAAAAAAADrJ8AQ/original"
-          alt="LTR"
-        />
-      }
+      label2={<RTLIcon css={dataDirectionIcon} />}
       tooltip2="RTL"
       pure
     />
