b175e21c7554d5cf46d0533343a3afbfe5ae2196
KuangPF <1633397286@qq.com>
2023-06-06 23:31:51 +0800

    style(Badge): 适配内置`Badge`组件状态颜色

--- src/common/styles/DumiDefaultStyleOverride.tsx
+++ src/common/styles/DumiDefaultStyleOverride.tsx
@@ -1,17 +1,43 @@
 import { css, Global } from '@emotion/react';
+import useSiteToken from '../../hooks/useSiteToken';
 
 const PREFERS_COLOR_ATTR = 'data-prefers-color';
+const prefix = 'dumi-default';
 
-export default () => (
-  <Global
-    styles={css`
-      [${PREFERS_COLOR_ATTR}=dark] {
-        .markdown {
-          img {
-            opacity: 0.8;
+export default () => {
+  const { token } = useSiteToken();
+  return (
+    <Global
+      styles={css`
+        [${PREFERS_COLOR_ATTR}=dark] {
+          .markdown {
+            img {
+              opacity: 0.8;
+            }
           }
         }
-      }
-    `}
-  />
-);
+        .dumi-theme-antd-root {
+          .${prefix}-badge {
+            &:not([type]),
+            &[type='info'] {
+              color: ${token.colorPrimary};
+              background: ${token.colorPrimaryBg};
+            }
+            &[type='warning'] {
+              color: ${token.colorWarning};
+              background: ${token.colorWarningBg};
+            }
+            &[type='success'] {
+              color: ${token.colorSuccess};
+              background: ${token.colorSuccessBg};
+            }
+            &[type='error'] {
+              color: ${token.colorError};
+              background: ${token.colorErrorBg};
+            }
+          }
+        }
+      `}
+    />
+  );
+};
