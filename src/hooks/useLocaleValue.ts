9847dfd1a4920bef741a901d2991612e47e93462
KuangPF <1633397286@qq.com>
2023-07-18 22:20:30 +0800

    fix(hooks): 兼容多语言模式取值 #17

--- src/hooks/useLocaleValue.ts
+++ src/hooks/useLocaleValue.ts
@@ -2,10 +2,10 @@ import { useLocale, useSiteData } from 'dumi';
 import type { IAllThemeConfig } from '../types';
 
 export default function useLocaleValue(key: string) {
-  const { themeConfig, locales } = useSiteData();
+  const { themeConfig } = useSiteData();
   const locale = useLocale();
 
   const additionalThemeConfig: IAllThemeConfig = themeConfig;
   const value = additionalThemeConfig[key];
-  return locales.length > 1 ? value?.[locale.id] : value;
+  return value?.[locale.id] ?? value;
 }
