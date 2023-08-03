94a9bc2a07586e99cc97abbcf7283e7c50316cc1
KuangPF <1633397286@qq.com>
2023-08-03 23:02:56 +0800

    chore(des): update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: ['@utopia/eslint-config-react', '@utopia/eslint-config-react/hooks']
+  extends: ['@utopia/eslint-config-react', '@utopia/eslint-config-react/hooks'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 0,
+    '@typescript-eslint/no-non-null-assertion': 0
+  }
 };
