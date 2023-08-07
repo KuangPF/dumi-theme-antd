8816639b567f0e70da4da84a1d6ee88e100464c3
KuangPF <1633397286@qq.com>
2023-08-07 23:22:27 +0800

    feat(utils): add `handleFullSidebarData` method

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { history, useSiteData } from 'dumi';
+import type { ISidebarGroup } from 'dumi/dist/client/theme-api/types';
 
 type ILocaleItem = ReturnType<typeof useSiteData>['locales'][0];
 
@@ -28,4 +29,52 @@ export const removeTitleCode = (title = '') => {
 
 export const isExternalLinks = (link?: string): boolean => {
   return /^(\w+:)\/\/|^(mailto|tel):/.test(link ?? '');
+};
+
+export const handleFullSidebarData = (
+  fullSidebarData: Record<string, ISidebarGroup[]>
+): Record<string, ISidebarGroup[]> => {
+  const nextFullSidebarData: Record<string, ISidebarGroup[]> = {};
+  Object.entries(fullSidebarData).forEach(([key, value]) => {
+    const existedLevelOneNavList = Object.keys(nextFullSidebarData);
+    if (key.split('/').length === 3) {
+      // 二级目录
+      const levelOneNavKey = `/${key.split('/')[1]}`;
+      const _levelTwoNav = value[0]?.children?.find((item) => item.frontmatter?.nav);
+      // 找找二级菜单名称以及顺序
+      const _levelTwoNavInfo = _levelTwoNav?.frontmatter?.nav;
+      const frontmatterNavSecond: { order: number; title?: string } = {
+        order: 0,
+        title: undefined
+      };
+      if (typeof _levelTwoNavInfo !== 'string') {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        frontmatterNavSecond.order = _levelTwoNavInfo?.second.order ?? 0;
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        frontmatterNavSecond.title = _levelTwoNavInfo?.second.title ?? _levelTwoNavInfo.second;
+      }
+
+      if (existedLevelOneNavList.includes(levelOneNavKey)) {
+        // 已存在，直接 push 为 children
+        nextFullSidebarData[levelOneNavKey].push({
+          title: frontmatterNavSecond.title,
+          order: frontmatterNavSecond.order,
+          children: value[0]?.children
+        });
+      } else {
+        nextFullSidebarData[levelOneNavKey] = value.map((item) => ({
+          ...item,
+          title: frontmatterNavSecond.title,
+          order: frontmatterNavSecond.order
+        }));
+      }
+    } else if (existedLevelOneNavList.includes(key)) {
+      nextFullSidebarData[key] = nextFullSidebarData[key].concat(value);
+    } else {
+      nextFullSidebarData[key] = value;
+    }
+  });
+  return nextFullSidebarData;
 };
