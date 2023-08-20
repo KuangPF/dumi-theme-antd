dc5b41376abcfa741f87a84fc1430103d5b87d57
KuangPF <1633397286@qq.com>
2023-08-20 18:16:31 +0800

    feat(site): 抽离首页，避免渲染出现短暂 404

---
+++ example/.dumi/pages/index/index.tsx
@@ -0,0 +1,14 @@
+/* .dumi/pages/index.tsx */
+import React from 'react';
+import HomeBaseLayout from '../../theme/layouts/HomePageLayout';
+
+const CustomHomePage = () => {
+  return (
+    <div>
+      <HomeBaseLayout />
+      <div>other content</div>
+    </div>
+  );
+};
+
+export default CustomHomePage;
