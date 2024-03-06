b7a184364aff200615031cec9374ee0b5ba87588
Jeason <jitong.zjt@alibaba-inc.com>
2024-03-06 17:31:43 +0800

    fix: custom zh home page not working

--- src/layouts/DocLayout/index.tsx
+++ src/layouts/DocLayout/index.tsx
@@ -32,10 +32,6 @@ const DocLayout: FC = () => {
   const { loading } = useSiteData();
   const { direction } = useContext(SiteContext);
 
-  const is404Home = useMemo(
-    () => pathname.startsWith('/index') && routeMeta.texts.length === 0,
-    [pathname, routeMeta]
-  );
   const content = useMemo(() => {
     if (
       ['', '/'].some((path) => path === pathname) ||
@@ -43,7 +39,7 @@ const DocLayout: FC = () => {
     ) {
       return (
         <React.Fragment>
-          {outlet && !is404Home ? outlet : <Homepage />}
+          {outlet && routeMeta.texts.length === 0 ? outlet : <Homepage />}
           <Footer />
         </React.Fragment>
       );
@@ -57,7 +53,7 @@ const DocLayout: FC = () => {
         <Outlet />
       </SidebarLayout>
     );
-  }, [routeMeta.frontmatter?.sidebar, outlet, pathname, is404Home]);
+  }, [outlet, pathname, routeMeta]);
 
   // handle hash change or visit page hash from Link component, and jump after async chunk loaded
   useEffect(() => {
