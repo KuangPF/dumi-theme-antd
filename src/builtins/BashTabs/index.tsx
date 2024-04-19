1d3b1b6110e37e80426c30380ccc7e927e861f20
KuangPF <me@kuangpf.com>
2024-04-20 00:31:12 +0800

    feat(deps): update `@types/react` version

--- src/builtins/BashTabs/index.tsx
+++ src/builtins/BashTabs/index.tsx
@@ -1,9 +1,32 @@
 /**
  * bash 选项卡切换组件
  */
+import { useMemo, FC } from 'react';
+import { Tabs } from 'antd';
+import type { TabsProps } from 'antd';
 
-const BashTabs = () => {
-  return null;
+interface TabItemsType {
+  key: string;
+  children: React.ReactNode;
+  label: string;
+  icon?: string;
+}
+
+interface BashTabsProps {
+  /** tab 配置项 */
+  tabItems?: TabItemsType[];
+  /** 默认 tab key */
+  defaultActiveKey?: string;
+}
+const BashTabs: FC<BashTabsProps> = (props) => {
+  const { tabItems, defaultActiveKey } = props;
+  const items: TabsProps['items'] = useMemo(() => {
+    return tabItems?.map((item) => {
+      return item;
+    });
+  }, [tabItems]);
+
+  return <Tabs className="antd-site-snippet" defaultActiveKey={defaultActiveKey} items={items} />;
 };
 
 export default BashTabs;
