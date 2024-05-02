0d9cda05791244120fcc173083922869fe03f03d
KuangPF <me@kuangpf.com>
2024-05-02 15:17:42 +0800

    feat(core): initialize `BashTabs` builtin component

--- src/builtins/BashTabs/index.tsx
+++ src/builtins/BashTabs/index.tsx
@@ -1,15 +1,22 @@
 /**
  * bash 选项卡切换组件
  */
-import { useMemo, FC } from 'react';
+import { useMemo, FC, useCallback, useContext } from 'react';
 import { Tabs } from 'antd';
+import { type Language } from 'prism-react-renderer';
 import type { TabsProps } from 'antd';
+import SourceCode from 'dumi/theme-default/builtins/SourceCode';
+import SiteContext from '../../slots/SiteContext';
+import type { SiteContextProps } from '../../slots/SiteContext';
+import type { ThemeName } from '../../common/ThemeSwitch';
 
 interface TabItemsType {
   key: string;
-  children: React.ReactNode;
+  children: string;
   label: string;
-  icon?: string;
+  iconSrc?: string;
+  iconRender?: FC<{ theme: ThemeName[] }>;
+  lang?: Language;
 }
 
 interface BashTabsProps {
@@ -20,11 +27,32 @@ interface BashTabsProps {
 }
 const BashTabs: FC<BashTabsProps> = (props) => {
   const { tabItems, defaultActiveKey } = props;
+  const { theme } = useContext<SiteContextProps>(SiteContext);
+
+  const renderLabel = useCallback(
+    (item: TabItemsType) => {
+      const { label, iconRender, iconSrc } = item;
+      return (
+        <span className="snippet-label">
+          {typeof iconRender === 'function' ? iconRender({ theme }) : null}
+          {iconSrc ? <img src={iconSrc} alt="iconSrc" /> : null}
+          {label}
+        </span>
+      );
+    },
+    [theme]
+  );
+
   const items: TabsProps['items'] = useMemo(() => {
     return tabItems?.map((item) => {
-      return item;
+      const { key = String(Date.now()), children, lang = 'bash' } = item;
+      return {
+        key,
+        children: children ? <SourceCode lang={lang}>{children}</SourceCode> : null,
+        label: renderLabel(item)
+      };
     });
-  }, [tabItems]);
+  }, [tabItems, renderLabel]);
 
   return <Tabs className="antd-site-snippet" defaultActiveKey={defaultActiveKey} items={items} />;
 };
